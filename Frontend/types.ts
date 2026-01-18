ea21b0bec9191cc8ad11a57fc3ad47071301476c
Dean David Menkis <deanandgecko@gmail.com>
2026-01-17 16:32:17 -0800

    .

--- Frontend/types.ts
+++ Frontend/types.ts
@@ -4,10 +4,20 @@ export interface CardState {
   name: string;
   email: string;
   jobTitle: string;
+  phoneNumber: string;
+  github: string;
+  linkedin: string;
+  showQrCode: boolean;
+  qrCodeLink: string;
   color: string;
   setName: (name: string) => void;
   setEmail: (email: string) => void;
   setJobTitle: (jobTitle: string) => void;
+  setPhoneNumber: (phone: string) => void;
+  setGithub: (github: string) => void;
+  setLinkedin: (linkedin: string) => void;
+  setShowQrCode: (show: boolean) => void;
+  setQrCodeLink: (link: string) => void;
   setColor: (color: string) => void;
 }
 
