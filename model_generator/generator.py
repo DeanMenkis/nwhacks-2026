3b35ce4e0b0fb81d19fa633eebbc68589b8e35e5
Ilia <iliamorozim@gmail.com>
2026-01-17 17:13:56 -0800

    text as a separete object added

--- model_generator/generator.py
+++ model_generator/generator.py
@@ -6,10 +6,10 @@ import numpy as np
 import trimesh
 
 BOX_EXTENTS = np.array([84.5, 54.0, 1.6])
-TEXT_CONTENT = "FUCK ME IN THE ASS"
-TEXT_HEIGHT = 8.0
-TEXT_MARGIN = 8.0
-TEXT_DEPTH = 1.5
+TEXT_CONTENT = "Stop it daddy <3"
+TEXT_HEIGHT = 4.0
+TEXT_MARGIN = 4.0
+TEXT_DEPTH = 0.4
 TEXT_FONT = "DejaVu Sans"
 OUTPUT_3MF = "output.3mf"
 
@@ -59,11 +59,55 @@ def boolean_difference_openscad(
 
         return trimesh.load(result_path, force="mesh")
 
-mesh = trimesh.creation.box(extents=BOX_EXTENTS)
-mesh.split()
+
+def text_mesh_openscad(
+    box_extents,
+    text,
+    text_height,
+    text_depth,
+    text_margin,
+    text_font,
+):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_path = Path(tmpdir)
+        result_path = tmp_path / "text.stl"
+        scad_path = tmp_path / "text.scad"
+
+        top_z = box_extents[2] / 2.0
+        text_x = -box_extents[0] / 2.0 + text_margin
+        text_y = 0.0
+        text_z = top_z - text_depth
+
+        scad_path.write_text(
+            "\n".join(
+                [
+                    "translate(["
+                    f"{text_x}, {text_y}, {text_z}"
+                    "])",
+                    "  linear_extrude(height="
+                    f"{text_depth}"
+                    ")",
+                    "    text("
+                    f"\"{text}\", size={text_height}, font=\"{text_font}\", halign=\"left\", valign=\"center\""
+                    ");",
+                ]
+            ),
+            encoding="utf-8",
+        )
+
+        subprocess.run(
+            ["openscad", "-o", str(result_path), str(scad_path)],
+            check=True,
+        )
+
+        return trimesh.load(result_path, force="mesh")
+
+
+box_mesh = trimesh.creation.box(extents=BOX_EXTENTS)
+box_mesh.split()
 
 try:
-    mesh = boolean_difference_openscad(
+    box_mesh = boolean_difference_openscad(
         box_extents=BOX_EXTENTS,
         text=TEXT_CONTENT,
         text_height=TEXT_HEIGHT,
@@ -71,9 +115,24 @@ try:
         text_margin=TEXT_MARGIN,
         text_font=TEXT_FONT,
     )
-    scene = trimesh.Scene(mesh)
+
+    text_mesh = text_mesh_openscad(
+        box_extents=BOX_EXTENTS,
+        text=TEXT_CONTENT,
+        text_height=TEXT_HEIGHT,
+        text_depth=TEXT_DEPTH,
+        text_margin=TEXT_MARGIN,
+        text_font=TEXT_FONT
+    )
+
+    scene = trimesh.Scene()
+
+    scene.add_geometry(box_mesh, node_name="box")
+    scene.add_geometry(text_mesh, node_name="text_fill")
+
+
 except FileNotFoundError as exc:
     raise RuntimeError("OpenSCAD not found in PATH.") from exc
-# scene.show()
+scene.show()
 
-scene.export(OUTPUT_3MF)
+# scene.export(OUTPUT_3MF)
